2d6a8aef2ecf0c7837b5a26fa49aa9b31f9acdc1
SolowinFXVI <julien.n.jacquet@protonmail.com>
2020-05-23 00:20:10 +0200

    Added pom and artifact

---
+++ src/main/java/org/uvsq21400579/App.java
@@ -0,0 +1,13 @@
+package org.uvsq21400579;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
